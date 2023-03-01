43daefdbb340393980556df35fac2c151ae6a521
haha <390519764@qq.com>
2023-03-01 11:14:23 +0800

    feat: 增加颜色提示文字

--- publish.sh
+++ publish.sh
@@ -1,16 +1,23 @@
 #!bin/bash
+# 定义颜色变量
+RED="\E[1;31m" # 红
+GREEN='\E[1;32m' # 绿
+YELLOW='\E[1;33m' # 黄
+BLUE='\E[1;34m' # 蓝
+PINK='\E[1;35m' # 粉红
+RES='\E[0m' # 清除颜色
 # pull current branch commit
 git pull 
 # get current branch name
 branch_name=`git symbolic-ref --short HEAD`
-echo "current branch: $branch_name"
+printf "${GREEN}current branch: $branch_name${RES}\r\n"
 git checkout rc/publish
 git pull
 # merge crrent branch to publish branch
 git merge $branch_name
-echo "merge success"
+printf "${GREEN}merge success${RES}\r\n"
 # push op
 git push
-echo "publish success"
+printf "${GREEN} publish success${RES}\r\n"
 git checkout $branch_name
 echo "done"
