ae20851bded5d455a6c6adce6ba773c15f0f0a53
haha <390519764@qq.com>
2023-03-01 11:47:45 +0800

    feat: 发布分支检测

--- publish.sh
+++ publish.sh
@@ -6,18 +6,26 @@ YELLOW='\E[1;33m' # 黄
 BLUE='\E[1;34m' # 蓝
 PINK='\E[1;35m' # 粉红
 RES='\E[0m' # 清除颜色
-# pull current branch commit
-git pull 
+# set publish branch
+publish_branch="rc/publish"
 # get current branch name
 branch_name=`git symbolic-ref --short HEAD`
 printf "${GREEN}current branch: $branch_name${RES}\r\n"
-git checkout rc/publish
-git pull
-# merge crrent branch to publish branch
-git merge $branch_name
-printf "${GREEN}merge success${RES}\r\n"
-# push op
-git push
-printf "${GREEN} publish success${RES}\r\n"
-git checkout $branch_name
-echo "done"
+printf "${GREEN}publish branch: $publish_branch${RES}\r\n"
+if test $branch_name != $publish_branch
+then 
+    # pull current branch commit
+    git pull 
+    git checkout rc/publish
+    git pull
+    # merge crrent branch to publish branch
+    git merge $branch_name
+    printf "${GREEN}merge success${RES}\r\n"
+    # push op
+    git push
+    printf "${GREEN} publish success${RES}\r\n"
+    git checkout $branch_name
+    echo "done"
+else 
+    printf "${RED}you are in publish branch${RES}\r\n"
+fi
