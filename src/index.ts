dcab8a04e810b8abcf57e6dfaf1a3c2b7c0a131e
haha <390519764@qq.com>
2023-03-02 17:52:44 +0800

    feat: node版本

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { program } from "commander"
+import chalk from 'chalk'
+import  enquirer from 'enquirer'
+import { simpleGit } from "simple-git"
+
+const { prompt }  = enquirer
+
+program
+    .option('--first')
+    .option('-s, --separator <char>')
+
+program.command('publish')
+    .description('publish package')
+    .action(async () => {
+        const log = console.log;
+        const git = simpleGit()
+        log(chalk.blue('start publish'))
+        const response: {question: boolean} = await prompt({
+            type: 'confirm',
+            name: 'question',
+            message: 'do you need password'
+        })
+        // git symbolic-ref --short HEAD
+        if(response.question)  {
+            console.log('log password')
+        }
+        // await git.pull()
+        // await git.push()
+        const branch_name = await git.raw(['symbolic-ref', '--short', 'HEAD'])
+        console.log(chalk.green('current branch:', branch_name))
+    })
+
+program.parse()
